b064217bc3ee3061bc8d10130917a8bb4da5efd0
Oleksandr Minaiev <minaev@gmail.com>
2021-11-18 16:59:45 +0200

    fixing bugs-4-1

---
+++ src/test/java/com/sasha/sqlpractice/service/PostServiceTest.java
@@ -0,0 +1,21 @@
+package com.sasha.sqlpractice.service;
+
+import junit.framework.TestCase;
+
+public class PostServiceTest extends TestCase {
+
+    public void testGetById() {
+    }
+
+    public void testUpdate() {
+    }
+
+    public void testDeleteById() {
+    }
+
+    public void testGetAll() {
+    }
+
+    public void testSaveNewPost() {
+    }
+}
