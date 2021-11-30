3f6d7cc3ae9772774660ac415dc643ad2ec430cf
Oleksandr Minaiev <minaev@gmail.com>
2021-11-30 12:30:41 +0200

    fixing bugs-4-11 plus test LabelService

--- src/test/java/com/sasha/sqlpractic/service/LabelServiceTest.java
+++ src/test/java/com/sasha/sqlpractic/service/LabelServiceTest.java
@@ -1,53 +1,66 @@
 package com.sasha.sqlpractic.service;
 
+
 import com.sasha.sqlpractic.model.Label;
 import com.sasha.sqlpractic.repository.LabelRepository;
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class LabelServiceTest extends TestCase {
-    Label label = new Label();
-    List<Label> labels = new ArrayList<>();
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
-    public void setUp(){
+public class LabelServiceTest {
+
+    private final LabelRepository labelRepository = mock(LabelRepository.class);
+    private LabelService serviceUnderTest = new LabelService(labelRepository);
+
+    @Test
+    public void testUpdate() {
+        Label label = getLabel();
+        when(labelRepository.update(any())).thenReturn(label);
+        assertEquals(serviceUnderTest.update(label), label);
+    }
+
+    @Test
+    public void testGetById() {
+        Label label = getLabel();
+        when(labelRepository.getById(anyInt())).thenReturn(label);
+        assertEquals(labelRepository.getById(label.getId()), label);
+    }
+
+    @Test
+    public void testDeleteById() {
+        doNothing().when(labelRepository).deleteById(anyInt());
+        serviceUnderTest.deleteById(anyInt());
+        Mockito.verify(labelRepository, Mockito.times(1)).deleteById(anyInt());
+    }
+
+    @Test
+    public void testGetAll() {
+        List<Label> labels = new ArrayList<>();
+        labels.add(getLabel());
+        when(labelRepository.getAll()).thenReturn(labels);
+        assertEquals(serviceUnderTest.getAll(), labels);
+    }
+
+    @Test
+    public void testSave() {
+        Label label = getLabel();
+        when(labelRepository.save(label)).thenReturn(label);
+        assertEquals(serviceUnderTest.saveNewLabel(label), label);
+    }
+
+    private Label getLabel(){
+        Label label = new Label();
         label.setId(1);
         label.setName("John");
-        labels.add(label);
-    }
-
-
-    public void testUpdate() {
-        LabelRepository labelRepositoryMock = Mockito.mock(LabelRepository.class);
-        LabelService labelService = new LabelService(labelRepositoryMock);
-        assertEquals(labelService.update(label), label);
-
-    }
-
-    public void testGetById() {
-        LabelRepository labelRepositoryMock = Mockito.mock(LabelRepository.class);
-        assertEquals(labelRepositoryMock.getById(label.getId()), label);
-
-    }
-
-    public void testDeleteById() {
-        LabelService labelService = new LabelService();
-        LabelRepository labelRepositoryMock = Mockito.mock(LabelRepository.class);
-        Mockito.verify(labelService, Mockito.times(1)).deleteById(label.getId());
-    }
-
-    public void testGetAll() {
-        LabelRepository labelRepositoryMock = Mockito.mock(LabelRepository.class);
-        assertEquals(labelRepositoryMock.getAll(), labels);
-
-
-    }
-
-    public void testSave() {
-        LabelRepository labelRepositoryMock = Mockito.mock(LabelRepository.class);
-        assertEquals(labelRepositoryMock.save(label), label);
-
+        return label;
     }
 }
+
