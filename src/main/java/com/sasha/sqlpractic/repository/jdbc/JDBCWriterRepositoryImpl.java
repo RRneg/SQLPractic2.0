44445d5a771cc9a9f5e717ac4deaf127b4d9c51c
Oleksandr Minaiev <minaev@gmail.com>
2021-12-21 15:37:15 +0200

    fixing code`s bugs Writer-line

--- src/main/java/com/sasha/sqlpractic/repository/jdbc/JDBCWriterRepositoryImpl.java
+++ src/main/java/com/sasha/sqlpractic/repository/jdbc/JDBCWriterRepositoryImpl.java
@@ -39,12 +39,12 @@ public class JDBCWriterRepositoryImpl implements WriterRepository {
         List<Post> posts = new ArrayList<>();
         String sql = "SELECT * FROM writers JOIN writer_posts ON writers.ID = writer_posts.WRITER_ID JOIN posts ON writer_posts.POST_ID=posts.ID JOIN post_labels ON posts.ID = post_labels.POST_ID JOIN labels ON post_labels.LABELS_ID = labels.ID WHERE posts.POST_STATUS not like \"DELETE\" and writers.ID = ?";
         try (PreparedStatement pstm = JdbcUtils.getPrStatement(sql)) {
-            pstm.setInt(1,id);
+            pstm.setInt(1, id);
             ResultSet rs = pstm.executeQuery();
-            if (rs.next()){
-writer.setId(rs.getInt(1));
-writer.setFirstName(rs.getString(2));
-writer.setLastName(rs.getString(3));
+            if (rs.next()) {
+                writer.setId(rs.getInt(1));
+                writer.setFirstName(rs.getString(2));
+                writer.setLastName(rs.getString(3));
                 List<Label> labels = new ArrayList<>();
                 labels.add(new Label(rs.getInt(13), rs.getString(14)));
                 posts.add(new Post(
@@ -80,11 +80,13 @@ writer.setLastName(rs.getString(3));
 
     public Writer update(Writer writer) {
         String sql = "UPDATE WRITERS SET FIRST_NAME =? , LAST NAME=? WHERE ID = ?";
+        String sql2 = String.format("DELETE FROM writer_labels where WRITERS_ID = %d", writer.getId());
         try (PreparedStatement pstm = JdbcUtils.getPrStatement(sql)) {
             pstm.setString(1, writer.getFirstName());
             pstm.setString(2, writer.getLastName());
             pstm.setInt(3, writer.getId());
             pstm.executeUpdate();
+            pstm.execute(sql2);
             insertPostsInWriterPosts(writer.getId(), writer.getPosts());
         } catch (SQLException e) {
             e.printStackTrace();
@@ -105,10 +107,10 @@ writer.setLastName(rs.getString(3));
 
     public List<Writer> getAll() {
         List<Writer> writers = new ArrayList<>();
-        String sql = "SELECT * FROM writers JOIN writer_posts ON writers.ID = writer_posts.WRITER_ID JOIN posts ON writer_posts.POST_ID=posts.ID JOIN post_labels ON posts.ID = post_labels.POST_ID JOIN labels ON post_labels.LABELS_ID = labels.ID WHERE posts.POST_STATUS not like \"DELETE\" order by writers.ID";
+        String sql = "SELECT * FROM writers JOIN writer_posts ON writers.ID = writer_posts.WRITER_ID JOIN posts ON writer_posts.POST_ID=posts.ID JOIN post_labels ON posts.ID = post_labels.POST_ID JOIN labels ON post_labels.LABELS_ID = labels.ID WHERE posts.POST_STATUS not like \"DELETE\" order by writers.ID, posts.ID";
         try (PreparedStatement pstm = JdbcUtils.getPrStatement(sql)) {
             ResultSet rs = pstm.executeQuery();
-            if (rs.next()){
+            while (rs.next()) {
                 List<Label> labels = new ArrayList<>();
                 labels.add(new Label(rs.getInt(13),
                         rs.getString(14)));
@@ -127,52 +129,55 @@ writer.setLastName(rs.getString(3));
                         rs.getString(3),
                         posts));
             }
-            while (rs.next()){
-                List<Label> labels = new ArrayList<>();
-                labels.add(new Label(rs.getInt(13),
-                        rs.getString(14)));
-
-                List<Post> posts = new ArrayList<>();
-                posts.add(new Post(rs.getInt(6),
-                        rs.getString(7),
-                        rs.getDate(8).toString(),
-                        rs.getDate(9).toString(),
-                        labels,
-                        PostStatus.valueOf(rs.getString(10))));
-
-                writers.add(new Writer(
-                        rs.getInt(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        posts));
-            }
-            }
-
-         catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return getAllProcessed(writers);
     }
 
     private List<Writer> getAllProcessed(List<Writer> writers) {
-        return writers;
-    }
+        int size = writers.size();
+        if (size==0 || size == 1){
+            return writers;
+        }
+        else {
+            int i = 1;
+            while (i != size) {
+                if (writers.get(i - 1).getId() == writers.get(i).getId()) {
+                    writers.get(i - 1).getPosts().addAll(writers.get(i).getPosts());
+                    writers.remove(i);
+                    size = size - 1;
+                } else {
+                    i = i + 1;
+                }
+            }
+                JDBCPostRepositoryImpl postRepository = new JDBCPostRepositoryImpl();
+                for (Writer writer: writers) {
+                    writer.setPosts(postRepository.getAllProcessed(writer.getPosts()));
+                }
+                return writers;
+            }
+
+        }
+
+
 
     public Writer save(Writer writer) {
         String sql = "INSERT WRITERS (FIRST_NAME, LAST_NAME) VALUE (?, ?)";
-        try(PreparedStatement pstm = JdbcUtils.getPrStatementBackId(sql)){
+        try (PreparedStatement pstm = JdbcUtils.getPrStatementBackId(sql)) {
             pstm.setString(1, writer.getFirstName());
             pstm.setString(2, writer.getLastName());
             pstm.execute();
             ResultSet rs = pstm.getGeneratedKeys();
             int id = 0;
-            if(rs.next()){
+            if (rs.next()) {
                 id = rs.getInt(1);
                 writer.setId(id);
             }
             insertPostsInWriterPosts(id, writer.getPosts());
-            }
-        catch (SQLException e){e.printStackTrace();}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return writer;
     }
 
